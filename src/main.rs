a93b012f1567f4a7580d68cb053240e5778d7263
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-18 22:19:53 -0500

    Print messages for new Vector3.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod vector3;
+
+use crate::vector3::Vector3;
+use crate::vector3::Point3;
+use crate::vector3::Color3;
+
 fn main() {
-    println!("Hello, world!");
+    let v = Vector3::new(3.4, 234.23, 9999.9);
+    println!("{:?}", v);
+
+    let p = Point3::new(3.4, 234.23, 9999.9);
+    println!("{:?}", p);
 }
