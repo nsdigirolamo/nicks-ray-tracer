f156fee3901daa7571707c0d6268e0dfaa2d8274
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-20 16:46:54 -0500

    Implemented new color feature.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-mod vector3;
+mod color;
 mod ray;
 mod sphere;
+mod vector3;
 
+use crate::color::Color;
 use crate::vector3::Point3;
 use crate::ray::Ray;
 use crate::sphere::Sphere;
@@ -22,12 +24,13 @@ fn main() {
     bottom_left_corner.y -= view_height / 2.0;
     bottom_left_corner.z -= view_distance;
 
-    let sphere = Sphere::new(Point3::new(0.0, 0.0, -3.0), 1.0);
+    let front = Sphere::new(Point3::new(0.0, -51.0, 0.0), 50.0);
     
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
     for row in (0..image_height).rev() {
+        eprint!("\r{} scanlines remaining...      ", row);
         for col in 0..image_width {
             let w_ratio = col as f64 / image_width as f64;
             let h_ratio = row as f64 / image_height as f64;
@@ -37,10 +40,18 @@ fn main() {
             
             let ray = Ray::new(origin, direction);
 
-            match ray.get_intersect(&sphere) {
-                Some(t) => println!("255 0 0"),
-                None    => println!("0 255 0"),
+            match ray.get_intersect(&front, 0.01, 100.0) {
+                Some(t) => {
+                    println!("{}", Color::new(1.0, 0.0, 0.0));
+                }
+                None => {
+                    let s = 0.5 * (1.0 + ray.direction.unit().y);
+                    let c1 = Color::new(0.5, 0.7, 1.0) * s;
+                    let c2 = Color::new(1.0, 1.0, 1.0) * (1.0 - s);
+                    println!("{}", c1 + c2);
+                },
             }
         }
     }
+    eprintln!();
 }
