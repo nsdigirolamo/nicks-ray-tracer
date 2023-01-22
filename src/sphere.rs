c616817a082c4279835cd1aa089564bb76068d15
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-21 20:59:05 -0500

    Added material to sphere struct.

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,16 +1,19 @@
+use crate::material::Material;
 use crate::vector3::Point3;
 
 #[derive(Default, Clone, Copy)]
 pub struct Sphere {
     pub center: Point3,
     pub radius: f64,
+    pub material: Material,
 }
 
 impl Sphere {
-    pub fn new(center: Point3, radius: f64) -> Self {
+    pub fn new(center: Point3, radius: f64, material: Material) -> Self {
         Self {
             center: center,
             radius: radius,
+            material: material,
         }
     }
 }
