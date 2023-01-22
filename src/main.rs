af2bead0c4e0843ea3a83a484ce0cda2efb09e98
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-22 18:37:53 -0500

    Refactored some code to make it work with the new camera struct.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use crate::material::Material;
 use crate::ray::Ray;
 use crate::sphere::Sphere;
 use crate::vector3::Point3;
+use crate::vector3::Vector3;
 
 use rand::Rng;
 
@@ -63,18 +64,24 @@ fn get_ray_color(ray: Ray, spheres: &Vec<Sphere>, depth: i32) -> Color {
     
 fn main() {
 
-    let image_height = 1080;
     let aspect_ratio = 16.0 / 9.0;
-    let position = Point3::new(0.0, 0.0, 0.0);
-    let cam = Camera::new(image_height, aspect_ratio, position, 1.0, 2.0);
+    let image_height = 1080;
+    let image_width = (image_height as f64 * aspect_ratio) as i32;
+    let vfov = 60.0;
+
+    let look_from = Vector3::new(-2.0, 2.0, 1.0);
+    let look_to = Vector3::new(0.0, 0.0, -1.0);
+    let up = Vector3::new(0.0, 1.0, 0.0);
+    
+    let cam = Camera::new(look_from, look_to, up, vfov, aspect_ratio);
 
     let ground_mat = Material::new(Color::new(0.8, 0.8, 0.0), None, None);
     let ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, ground_mat);
 
-    let center_mat = Material::new(Color::new(1.0, 1.0, 1.0), None, Some(1.5));
+    let center_mat = Material::new(Color::new(0.8, 0.8, 0.8), None, None);
     let center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, center_mat);
 
-    let left_mat = Material::new(Color::new(0.8, 0.8, 0.8), Some(0.3), None);
+    let left_mat = Material::new(Color::new(1.0, 1.0, 1.0), Some(0.3), Some(1.5));
     let left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, left_mat);
 
     let right_mat = Material::new(Color::new(0.8, 0.6, 0.2), Some(1.0), None);
@@ -87,12 +94,12 @@ fn main() {
     let max_bounce_depth = 50;
 
     println!("P3");
-    println!("{} {}", cam.image_width, cam.image_height);
+    println!("{} {}", image_width, image_height);
     println!("255");
 
-    for row in (0..cam.image_height).rev() {
+    for row in (0..image_height).rev() {
         eprint!("\r{} scanlines remaining...      ", row);
-        for col in 0..cam.image_width {
+        for col in 0..image_width {
             
             let mut pixel_color = Color::new(0.0, 0.0, 0.0);
 
@@ -100,8 +107,8 @@ fn main() {
                 
                 let rand_w: f64 = rng.gen_range(-1.0..1.0);
                 let rand_h: f64 = rng.gen_range(-1.0..1.0);
-                let w = (col as f64 + rand_w) / cam.image_width as f64;
-                let h = (row as f64 + rand_h) / cam.image_height as f64;
+                let w = (col as f64 + rand_w) / image_width as f64;
+                let h = (row as f64 + rand_h) / image_height as f64;
                 
                 let ray = cam.get_ray(w, h);
                 pixel_color += get_ray_color(ray, &spheres, max_bounce_depth);
