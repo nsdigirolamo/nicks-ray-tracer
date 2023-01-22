34d8f94462ed886fd13e88986b9f7c75dc5faf75
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-22 17:40:19 -0500

    Added new refraction field to material.

--- src/material.rs
+++ src/material.rs
@@ -4,10 +4,15 @@ use crate::color::Color;
 pub struct Material {
     pub albedo: Color,
     pub reflectivity: Option<f64>,
+    pub refraction_index: Option<f64>,
 }
 
 impl Material {
-    pub fn new(albedo: Color, reflectivity: Option<f64>) -> Self {        
-        Self { albedo: albedo, reflectivity: reflectivity }
+    pub fn new(albedo: Color, reflectivity: Option<f64>, refraction_index: Option<f64>) -> Self {        
+        Self { 
+            albedo: albedo, 
+            reflectivity: reflectivity,
+            refraction_index: refraction_index 
+        }
     }
 }
