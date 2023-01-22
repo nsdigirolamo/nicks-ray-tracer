a1a0a42f93101917a1936645012af3086aac922e
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-21 22:33:59 -0500

    Added reflectivity to materials.

--- src/material.rs
+++ src/material.rs
@@ -3,10 +3,11 @@ use crate::color::Color;
 #[derive(Default, Clone, Copy)]
 pub struct Material {
     pub albedo: Color,
+    pub reflectivity: Option<f64>,
 }
 
 impl Material {
-    pub fn new(albedo: Color) -> Self {        
-        Self { albedo: albedo, }
+    pub fn new(albedo: Color, reflectivity: Option<f64>) -> Self {        
+        Self { albedo: albedo, reflectivity: reflectivity }
     }
 }
