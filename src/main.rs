4cbcbecef398e3e570c7865f3cb7d4f43a5f4acf
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-22 21:35:03 -0500

    Set up the final scene for the first book.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod sphere;
 mod vector3;
 
 use crate::camera::Camera;
-use crate::color::Color;
+use crate::color::*;
 use crate::hit::Hit;
 use crate::material::Material;
 use crate::ray::Ray;
@@ -67,33 +67,66 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_height = 1080;
     let image_width = (image_height as f64 * aspect_ratio) as i32;
-    let vfov = 60.0;
+    let vfov = 20.0;
 
-    let look_from = Vector3::new(3.0, 3.0, 2.0);
-    let look_to = Vector3::new(0.0, 0.0, -1.0);
+    let look_from = Vector3::new(13.0, 2.0, 4.0);
+    let look_to = Vector3::new(0.0, 0.5, 0.0);
     let up = Vector3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (look_from - look_to).mag();
-    let aperature = 2.0;
+    let dist_to_focus = 10.0;
+    let aperature = 0.1;
     
     let cam = Camera::new(look_from, look_to, up, vfov, aspect_ratio, aperature, dist_to_focus);
 
-    let ground_mat = Material::new(Color::new(0.8, 0.8, 0.0), None, None);
-    let ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, ground_mat);
+    let mut spheres = Vec::new();
 
-    let center_mat = Material::new(Color::new(0.8, 0.8, 0.8), None, None);
-    let center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, center_mat);
-
-    let left_mat = Material::new(Color::new(1.0, 1.0, 1.0), Some(0.3), Some(1.5));
-    let left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, left_mat);
-
-    let right_mat = Material::new(Color::new(0.8, 0.6, 0.2), Some(1.0), None);
-    let right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, right_mat);
+    let ground_mat = Material::new(_GREY, None, None);
+    let ground = Sphere::new(Point3::new(0.0, -1000.0, -1.0), 1000.0, ground_mat);
+    spheres.push(ground);
     
-    let spheres = vec![ground, center, left, right];
+    let radius = 1.0;
+
+    let left_mat = Material::new(_LIGHT_RED, None, None);
+    let left = Sphere::new(Point3::new(-3.0, radius, 0.0), radius, left_mat);
+    spheres.push(left);
+
+    let center_mat = Material::new(_WHITE, None, Some(1.5));
+    let center = Sphere::new(Point3::new(0.0, radius, 0.0), radius, center_mat);
+    spheres.push(center);
+
+    let right_mat = Material::new(_LIGHT_BLUE, Some(0.05), None);
+    let right = Sphere::new(Point3::new(3.0, radius, 0.0), radius, right_mat);
+    spheres.push(right);
 
     let mut rng = rand::thread_rng();
-    let samples_per_pixel = 100;
-    let max_bounce_depth = 50;
+
+    for x in -15..10 {
+        for z in -10..5 {
+
+            let radius = 0.15;
+            let center = Point3::new(x as f64, radius, z as f64);
+        
+            let r = rng.gen();
+            let c = Color::new(rng.gen(), rng.gen(), rng.gen());
+            let mut material = Material::new(c, None, None);
+
+            if 0.75 < r {
+
+                material = Material::new(c, Some(rng.gen_range(0.0..1.0)), None);
+
+            } else if 0.5 < r{
+
+                material = Material::new(c, None, Some(rng.gen_range(1.0..2.0)));
+
+            }
+
+            let sphere = Sphere::new(center, radius, material);
+            spheres.push(sphere);
+
+        }
+    }
+
+    let samples_per_pixel = 500;
+    let max_bounce_depth = 100;
 
     println!("P3");
     println!("{} {}", image_width, image_height);
