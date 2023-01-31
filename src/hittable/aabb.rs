89f1250ebbf4bf7690d50b5a9dc3c3c52eccdb2d
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-30 20:06:27 -0500

    Added axis-aligned bounding boxes.

---
+++ src/hittable/aabb.rs
@@ -0,0 +1,65 @@
+use crate::vector3::Point3
+
+/// Represents an axis-aligned bounding box in 3D space.
+pub struct AxisAlignedBoundingBox {
+    /// The minimum corner of the bounding box.
+    min: Point3,
+    /// The maximum corner of the bounding box.
+    max: Point3,
+}
+
+/// Acronym for AxisAlignedBoundingBox.
+pub type AABB = AxisAlignedBoundingBox;
+
+impl AABB {
+
+    ///
+    /// Returns an AABB constructed from the given arguments.
+    ///
+    /// # Arguments
+    /// * `min` - The AABB's min field.
+    /// * `max` - The AABB's max field.
+    ///
+    pub fn new(min: Point3, max: Poing3) -> Self {
+        Self {
+            min: min,
+            max: max,
+        }
+    }
+}
+
+impl Hittable for AABB {
+
+    ///
+    /// Returns true if there is a Hit between the AABB and a given Ray.
+    ///
+    /// # Arguments
+    /// * `&self` - The AABB intersected by the Ray.
+    /// * `ray` - The intersecting Ray.
+    /// * `min_dist` - The minimum distance along the Ray to check for a Hit.
+    /// * `max_dist` - The maximum distance along the Ray to check for a Hit.
+    ///
+    fn is_hit(&self, ray: Ray, min_dist: f64, max_dist: f64) -> bool {
+
+        let inverse_x_direction = ray.direction.x;
+        let x_min_dist = (self.min.x - ray.origin.x) * inverse_x_direction;
+        let x_max_dist = (self.max.x - ray.origin.x) * inverse_x_direction;
+        if (inverse_x_direction < 0.0) { swap(x_min_dist, x_max_dist) }
+
+        let inverse_y_direction = ray.direction.y;
+        let y_min_dist = (self.min.y - ray.origin.y) * inverse_y_direction;
+        let y_max_dist = (self.max.y - ray.origin.y) * inverse_y_direction;
+        if (inverse_y_direction < 0.0) { swap(y_min_dist, y_max_dist) }
+
+        let inverse_z_direction = ray.direction.z;
+        let z_min_dist = (self.min.z - ray.origin.z) * inverse_z_direction;
+        let z_max_dist = (self.max.z - ray.origin.z) * inverse_z_direction;
+        if (inverse_z_direction < 0.0) { swap(z_min_dist, z_max_dist) }
+
+        let xy_overlap = x_min_dist < y_max_dist && y_min_dist < x_max_dist;
+        let yz_overlap = y_min_dist < z_max_dist && z_min_dist < y_max_dist;
+        let zx_overlap = z_min_dist < x_max_dist && x_min_dist < z_max_dist;
+
+        xy_overlap && yz_overlap && zx_overlap
+    }
+}
