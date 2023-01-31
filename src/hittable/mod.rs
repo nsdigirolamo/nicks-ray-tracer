029a37e65140a1c17cb8373f29cb13c7bc5f85ca
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-01-30 19:44:00 -0500

    Added a new function for the Hittable trait.

--- src/hittable/mod.rs
+++ src/hittable/mod.rs
@@ -3,18 +3,17 @@ pub mod sphere;
 use crate::Hit;
 use crate::Ray;
 
-/// Represents any object that can be hit by a Ray.
+/// Represents any object that can be Hit by a Ray.
 pub trait Hittable {
 
-    ///
-    /// Returns a Hit if there is an intersection between the hittable and the
-    /// given ray. If no such intersection exists, return None.
-    ///
-    /// # Arguments
-    /// * `&self` - The hittable that the ray possibly intersects.
-    /// * `ray` - The ray that is possibly intersecting the hittable.
-    /// * `min_dist` - The minimum distance along the ray to look for an intersection.
-    /// * `max_dist` - The maximum distance along the ray to look for an intersection.
-    ///
-    fn get_intersect(&self, ray: Ray, min_dist: f64, max_dist: f64) -> Option<Hit>;
+    fn get_hit(&self, ray: Ray, min_dist: f64, max_dist: f64) -> Option<Hit> {
+        None
+    }
+
+    fn is_hit(&self, ray: Ray, min_dist: f64, max_dist: f64) -> bool {
+        match self.get_hit(ray, min_dist, max_dist) {
+            Some(_) => true,
+            None => false,
+        }
+    }
 }
