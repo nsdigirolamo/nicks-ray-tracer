9bf2cf2620448b4e64db173b2d5d40154fd67bdf
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-02-01 23:07:35 -0500

    Changed material structs to store textures instead of colors.

--- src/material.rs
+++ src/material.rs
@@ -1,10 +1,9 @@
-use crate::color::Color;
+use crate::texture::Texture;
 
-/// Represents the material of an object.
-#[derive(Default, Clone, Copy)]
+/// Represents the material of a Hittable
 pub struct Material {
-    /// The material's albedo.
-    pub albedo: Color,
+    /// The material's texture.
+    pub texture: Box<dyn Texture>,
     /// The material's reflectivity, if applicable.
     pub reflectivity: Option<f64>,
     /// The material's refraction index, if applicable.
@@ -21,9 +20,9 @@ impl Material {
     /// * `reflectivity` - The material's reflectivity, if applicable.
     /// * `refraction_index` - The material's refraction index, if applicable.
     ///
-    pub fn new(albedo: Color, reflectivity: Option<f64>, refraction_index: Option<f64>) -> Self {        
+    pub fn new(texture: Box<dyn Texture>, reflectivity: Option<f64>, refraction_index: Option<f64>) -> Self {        
         Self { 
-            albedo: albedo, 
+            texture: texture, 
             reflectivity: reflectivity,
             refraction_index: refraction_index 
         }
