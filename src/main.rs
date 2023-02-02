0e7d9c4a46a4e29d1ff45c34d30c3a38efd80dea
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-02-02 12:18:53 -0500

    Moved the scene construction to a new function in scene.rs

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,12 @@ mod vector3;
 use crate::camera::Camera;
 use crate::color::*;
 use crate::hit::Hit;
-use crate::hittable::sphere::Sphere;
-use crate::material::Material;
-use crate::texture::monochrome::Monochrome;
 use crate::ray::Ray;
 use crate::scene::Scene;
-use crate::vector3::Point3;
+use crate::scene::construct_book1_final;
 use crate::vector3::Vector3;
 
 use rand::Rng;
-use std::rc::Rc;
 
 ///
 /// Returns the Color of a ray. Checks to see if the given ray intersects with
@@ -72,57 +68,7 @@ fn main() {
 
     let cam = Camera::new(look_from, look_to, up, vfov, aspect_ratio, aperature, dist_to_focus);
 
-    let ground_texture = Monochrome::new(_GREY);
-    let ground_material = Material::new(Box::new(ground_texture), None, None);
-    let ground = Sphere::new(Point3::new(0.0, -1000.0, -1.0), 1000.0, ground_material);
-
-    let mut scene = Scene::new(Rc::new(ground));
-
-    let radius = 1.0;
-
-    let left_texture = Monochrome::new(_LIGHT_RED);
-    let left_material = Material::new(Box::new(left_texture), None, None);
-    let left = Sphere::new(Point3::new(-3.0, radius, 0.0), radius, left_material);
-    scene.push(Rc::new(left));
-
-    let center_texture = Monochrome::new(_WHITE);
-    let center_material = Material::new(Box::new(center_texture), None, Some(1.5));
-    let center = Sphere::new(Point3::new(0.0, radius, 0.0), radius, center_material);
-    scene.push(Rc::new(center));
-
-    let right_texture = Monochrome::new(_LIGHT_BLUE);
-    let right_material = Material::new(Box::new(right_texture), Some(0.05), None);
-    let right = Sphere::new(Point3::new(3.0, radius, 0.0), radius, right_material);
-    scene.push(Rc::new(right));
-
-    let mut rng = rand::thread_rng();
-
-    for x in -15..10 {
-        for z in -10..5 {
-
-            let radius = 0.15;
-            let center = Point3::new(x as f64, radius, z as f64);
-
-            let r = rng.gen();
-            let color = Color::new(rng.gen(), rng.gen(), rng.gen());
-            let texture = Monochrome::new(color);
-            let mut material = Material::new(Box::new(texture), None, None);
-
-            if 0.75 < r {
-
-                material = Material::new(Box::new(texture), Some(rng.gen_range(0.0..1.0)), None);
-
-            } else if 0.5 < r{
-
-                material = Material::new(Box::new(texture), None, Some(rng.gen_range(1.0..2.0)));
-
-            }
-
-            let sphere = Sphere::new(center, radius, material);
-            scene.push(Rc::new(sphere));
-
-        }
-    }
+    let scene = construct_book1_final();
 
     let samples_per_pixel = 10;
     let max_bounce_depth = 10;
@@ -130,6 +76,8 @@ fn main() {
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
+
+    let mut rng = rand::thread_rng();
 
     for row in (0..image_height).rev() {
         eprint!("\r{} scanlines remaining...      ", row);
